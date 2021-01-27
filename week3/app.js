27843690ad8270efa167fdf4e65401e2e251b104
CopperWaffles <tylerrubenstein11510@gmail.com>
2021-01-27 13:32:52 -0500

    week 3 day one

---
+++ week3/app.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express()
+var serv = require('http').Server(app)
+var io = require('socket.io')(serv,{})
+
+//file comms
+app.get('/',function(req,res)
+{
+    res.sendFile(__dirname+'/client/index.html')
+})
+
+app.use('/client',express.static(__dirname+'/client'))
+
+serv.listen(3000, function()
+{
+    console.log('Connected on Port 3000')
+})
+
+//server side comms
+io.sockets.on('connection', function(socket)
+{
+    console.log('Socket Connected')
+
+    socket.on('sendMsg', function(data)
+    {
+        console.log(data.message)
+    })
+    socket.on('sendBtnMsg',function(data)
+    {
+        console.log(data.message)
+    })
+
+    socket.emit('messageFromServer',{
+        message:'Hey Jordan'
+    })
+})
